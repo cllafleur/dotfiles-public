d4a5e900e9a0b954b8be288f24fd3208754ea350
clement <cllafleur@gmail.com>
2025-12-01 17:41:00 +0100

    adding shortcut to make window floating & sticky

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,7 @@ vim.filetype.add({
 		jinja = "jinja",
 		jinja2 = "jinja",
 		j2 = "jinja",
+		csproj = "xml",
 	},
 })
 
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -330,4 +330,15 @@ return {
 			}
 		end,
 	},
+	{
+		"conform.nvim",
+		opts = function()
+			local opts = {
+				formatters_by_ft = {
+					xml = { "xmlformat" },
+				},
+			}
+			return opts
+		end,
+	},
 }
