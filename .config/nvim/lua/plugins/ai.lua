20e351f4ec64373ec192a60107ccdb7c025cd8a4
clement <cllafleur@gmail.com>
2026-03-19 18:08:04 +0100

    updating codecompanion config to use copilot acp by default

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"Davidyz/VectorCode",
+		enabled = false,
 		version = "*", -- optional, depending on whether you're on nightly or release
 		--build = "uv tool upgrade vectorcode",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -97,128 +98,6 @@ return {
 			})
 		end,
 	},
-	--{
-	--	"Kurama622/llm.nvim",
-	--	enabled = false,
-	--	dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" },
-	--	cmd = { "LLMSessionToggle", "LLMSelectedTextHandler", "LLMAppHandler" },
-	--	config = function()
-	--		local tools = require("llm.tools")
-	--		vim.fn.setenv("LLM_KEY", "")
-	--		require("llm").setup({
-	--			url = "http://localhost:11434/api/chat",
-	--			model = "qwen2.5-coder:7b",
-	--			api_type = "ollama",
-	--			temperature = 0.3,
-	--			top_p = 0.7,
-
-	--			spinner = {
-	--				text = {
-	--					"|--",
-	--					"-|-",
-	--					"--|",
-	--					"-|-",
-	--				},
-	--				hl = "Title",
-	--			},
-	--			prefix = {
-	--				user = { text = "😃 ", hl = "Title" },
-	--				assistant = { text = "  ", hl = "Added" },
-	--			},
-
-	--			history_path = "/tmp/llm-history",
-	--			save_session = true,
-	--			max_history = 15,
-	--			max_history_name_length = 20,
-
-	--      -- stylua: ignore
-	--      keys = {
-	--        -- The keyboard mapping for the input window.
-	--        ["Input:Submit"]      = { mode = "n", key = "<cr>" },
-	--        ["Input:Cancel"]      = { mode = {"n", "i"}, key = "<C-c>" },
-	--        ["Input:Resend"]      = { mode = {"n", "i"}, key = "<C-r>" },
-
-	--        -- only works when "save_session = true"
-	--        ["Input:HistoryNext"] = { mode = {"n", "i"}, key = "<C-j>" },
-	--        ["Input:HistoryPrev"] = { mode = {"n", "i"}, key = "<C-k>" },
-
-	--        -- The keyboard mapping for the output window in "split" style.
-	--        ["Output:Ask"]        = { mode = "n", key = "i" },
-	--        ["Output:Cancel"]     = { mode = "n", key = "<C-c>" },
-	--        ["Output:Resend"]     = { mode = "n", key = "<C-r>" },
-
-	--        -- The keyboard mapping for the output and input windows in "float" style.
-	--        ["Session:Toggle"]    = { mode = "n", key = "<leader>ac" },
-	--        ["Session:Close"]     = { mode = "n", key = {"<esc>", "Q"} },
-
-	--        -- Scroll
-	--        ["PageUp"]            = { mode = {"i","n"}, key = "<C-b>" },
-	--        ["PageDown"]          = { mode = {"i","n"}, key = "<C-f>" },
-	--        ["HalfPageUp"]        = { mode = {"i","n"}, key = "<C-u>" },
-	--        ["HalfPageDown"]      = { mode = {"i","n"}, key = "<C-d>" },
-	--        ["JumpToTop"]         = { mode = "n", key = "gg" },
-	--        ["JumpToBottom"]      = { mode = "n", key = "G" },
-	--      },
-
-	--			app_handler = {
-	--				Completion = {
-	--					handler = tools.completion_handler,
-	--					opts = {
-	--						url = "http://localhost:11434/v1/completions",
-	--						model = "qwen2.5-coder:7b",
-	--						api_type = "ollama",
-	--						n_completions = 3,
-	--						context_window = 512,
-	--						max_tokens = 256,
-
-	--						-- A mapping of filetype to true or false, to enable completion.
-	--						filetypes = { sh = false },
-
-	--						-- Whether to enable completion of not for filetypes not specifically listed above.
-	--						default_filetype_enabled = true,
-
-	--						auto_trigger = true,
-
-	--						-- just trigger by { "@", ".", "(", "[", ":", " " } for `style = "nvim-cmp"`
-	--						only_trigger_by_keywords = true,
-
-	--						style = "virtual_text", -- nvim-cmp or blink.cmp
-
-	--						timeout = 10, -- max request time
-
-	--						-- only send the request every x milliseconds, use 0 to disable throttle.
-	--						throttle = 1000,
-	--						-- debounce the request in x milliseconds, set to 0 to disable debounce
-	--						debounce = 400,
-	--						keymap = {
-	--							virtual_text = {
-	--								accept = {
-	--									mode = "i",
-	--									keys = "<C-l>",
-	--								},
-	--								next = {
-	--									mode = "i",
-	--									keys = "<A-n>",
-	--								},
-	--								prev = {
-	--									mode = "i",
-	--									keys = "<A-p>",
-	--								},
-	--								toggle = {
-	--									mode = "n",
-	--									keys = "<leader>cp",
-	--								},
-	--							},
-	--						},
-	--					},
-	--				},
-	--			},
-	--		})
-	--	end,
-	--	keys = {
-	--		{ "<leader>ac", mode = "n", "<cmd>LLMSessionToggle<cr>" },
-	--	},
-	--},
 	{
 		"olimorris/codecompanion.nvim",
 		opts = {
@@ -226,11 +105,14 @@ return {
 		},
 		config = function()
 			require("codecompanion").setup({
-				strategies = {
-					inline = { adapter = { name = "copilot", model = "gpt-5-mini" } },
-					chat = { adapter = { name = "copilot", model = "gpt-5-mini" } },
+				interactions = {
+					inline = { adapter = { name = "copilot", model = "gpt-5.4-mini" } },
+					chat = { adapter = "copilot_acp" },
 					-- chat = { adapter = { name = "copilot", model = "claude-sonnet-4.5" } },
-					cmd = { adapter = { name = "copilot", model = "gpt-5-mini" } },
+					cmd = { adapter = { name = "copilot", model = "gpt-5.4-mini" } },
+					cli = {
+						adpater = "copilot_acp",
+					},
 				},
 				memory = {
 					opts = {
@@ -240,8 +122,15 @@ return {
 						},
 					},
 				},
-				prompt_library = require("plugins.extensions.companion-prompt-library").get_prompt_library(),
+				--prompt_library = require("plugins.extensions.companion-prompt-library").get_prompt_library(),
 				adapters = {
+					acp = {
+						copilot_acp = function()
+							return require("codecompanion.adapters").extend("copilot_acp", {
+								defaults = { mcpServers = "inherit_from_config" },
+							})
+						end,
+					},
 					http = {
 						gemini = function()
 							return require("codecompanion.adapters").extend("gemini", {
@@ -313,85 +202,8 @@ return {
 						end,
 					},
 				},
-				extensions = {
-					vectorcode = {
-						---@type VectorCode.CodeCompanion.ExtensionOpts
-						opts = {
-							tool_group = {
-								-- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
-								enabled = true,
-								-- a list of extra tools that you want to include in `@vectorcode_toolbox`.
-								-- if you use @vectorcode_vectorise, it'll be very handy to include
-								-- `file_search` here.
-								extras = {},
-								collapse = false, -- whether the individual tools should be shown in the chat
-							},
-							tool_opts = {
-								---@type VectorCode.CodeCompanion.ToolOpts
-								["*"] = {},
-								---@type VectorCode.CodeCompanion.LsToolOpts
-								ls = {},
-								---@type VectorCode.CodeCompanion.VectoriseToolOpts
-								vectorise = {},
-								---@type VectorCode.CodeCompanion.QueryToolOpts
-								query = {
-									max_num = { chunk = -1, document = 4 },
-									default_num = { chunk = 50, document = 10 },
-									include_stderr = false,
-									use_lsp = true,
-									no_duplicate = true,
-									chunk_mode = true,
-									---@type VectorCode.CodeCompanion.SummariseOpts
-									summarise = {
-										---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
-										enabled = false,
-										adapter = nil,
-										query_augmented = true,
-									},
-								},
-								files_ls = {},
-								files_rm = {},
-							},
-						},
-					},
-					mcphub = {
-						callback = "mcphub.extensions.codecompanion",
-						opts = {
-							make_vars = true,
-							make_slash_commands = true,
-							show_result_in_chat = true,
-						},
-					},
-				},
+				extensions = {},
 			})
-			--local progress = require("fidget.progress")
-			--local handles = {}
-			--local group = vim.api.nvim_create_augroup("CodeCompanionFidget", {})
-
-			--vim.api.nvim_create_autocmd("User", {
-			--	pattern = "CodeCompanionRequestStarted",
-			--	group = group,
-			--	callback = function(e)
-			--		handles[e.data.id] = progress.handle.create({
-			--			title = "CodeCompanion",
-			--			message = "Thinking...",
-			--			lsp_client = { name = e.data.adapter.formatted_name },
-			--		})
-			--	end,
-			--})
-
-			--vim.api.nvim_create_autocmd("User", {
-			--	pattern = "CodeCompanionRequestFinished",
-			--	group = group,
-			--	callback = function(e)
-			--		local h = handles[e.data.id]
-			--		if h then
-			--			h.message = e.data.status == "success" and "Done" or "Failed"
-			--			h:finish()
-			--			handles[e.data.id] = nil
-			--		end
-			--	end,
-			--})
 		end,
 		init = function()
 			require("plugins.extensions.companion-notification").init()
@@ -401,21 +213,7 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 			"folke/noice.nvim",
 			--"j-hui/fidget.nvim",
-			"ravitemer/mcphub.nvim",
 		},
-	},
-	{
-		"ravitemer/mcphub.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		build = "bundled_build.lua",
-		config = function()
-			require("mcphub").setup({
-				port = 37373,
-				use_bundled_binary = true,
-			})
-		end,
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
